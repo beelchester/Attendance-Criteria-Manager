b766f34abe464817f6604e27d9c6caf282d1633b
sahilyeole <sahilyeole93@gmail.com>
2022-09-16 18:37:51 +0530

    added form

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,55 @@
-import {useState} from 'react'
-import Popup from './Popup';
+import { useState } from "react";
+import Popup from "./Popup";
 
 const Card = () => {
   const [editCard, setEditCard] = useState(false);
 
   function toggleEditCard() {
-    setEditCard(prev => !prev);
+    setEditCard((prev) => !prev);
   }
-  const editPopup = editCard ? <Popup >
-    <button className='w-20 h-12 bg-accent rounded-lg font-poppins font-semibold' onClick={toggleEditCard} >click</button>
-  </Popup> : "";
+  const editPopup = editCard ? (
+    <Popup>
+      <form className="flex flex-col ">
+      <h3 className="text-white font-poppins font-semibold">Subject Name:</h3>
+        <input type="text" className="rounded px-3 mt-3 mb-6" />
+        <h3 className="text-white font-poppins font-semibold">
+          Classes Attended:
+        </h3>
+        <div className="flex items-center justify-start mb-6">
+          <input type="text" className="rounded px-3 my-3 mr-3 w-16" />
+          <h3 className="text-white font-poppins"> Out Of</h3>
+          <input type="text" className="rounded px-3 ml-3 my-3 w-16" />
+        </div>
+      <button
+        className="w-20 h-12 bg-accent rounded-lg font-poppins font-semibold"
+        onClick={toggleEditCard}
+        >
+        click
+      </button>
+        </form>
+    </Popup>
+  ) : (
+    ""
+  );
   return (
-    <div className='h-[18rem] w-[18rem] bg-[#090d11bd] rounded-xl ring ring-[#0000007c] p-3 text-center'>
+    <div className="h-[18rem] w-[18rem] bg-[#090d11bd] rounded-xl ring ring-[#0000007c] p-3 text-center">
       {editPopup}
-      <h1 className='text-[#a0c4f0] font-poppins font-semibold uppercase text-3xl mt-2  '>DSA</h1>
+      <h1 className="text-[#a0c4f0] font-poppins font-semibold uppercase text-3xl mt-2  ">
+        DSA
+      </h1>
       {/* <h3 className='text-greenT font-sans font-semibold'>82%</h3> */}
-      <h3 className='text-redT font-sans font-semibold mt-8'>68%</h3>
-      <h3 className=' font-sans font-semibold my-3 text-white'>You need to attend the next 3 classes</h3>
-      <button onClick={toggleEditCard} className='bg-[#fee715e3] h-10 px-4 text-sm mt-9 rounded-lg font-poppins font-semibold'>Edit</button>
+      <h3 className="text-redT font-sans font-semibold mt-8">68%</h3>
+      <h3 className=" font-sans font-semibold my-3 text-white">
+        You need to attend atleast the next 3 classes
+      </h3>
+      <button
+        onClick={toggleEditCard}
+        className="bg-[#fee715e3] h-10 px-4 text-sm mt-9 rounded-lg font-poppins font-semibold"
+      >
+        Edit
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,32 @@ const Navbar = () => {
   const [addSub, setAddSub] = useState(false);
 
   function toggleAddSub() {
-    setAddSub(prev => !prev);
+    setAddSub((prev) => !prev);
   }
-  const subPopup = addSub ? <Popup >
-    <button className='w-20 h-12 bg-accent rounded-lg font-poppins font-semibold' onClick={toggleAddSub} >click</button>
-  </Popup> : "";
+  const subPopup = addSub ? (
+    <Popup>
+      <form className="flex flex-col ">
+        <h3 className="text-white font-poppins font-semibold">Subject Name:</h3>
+        <input type="text" className="rounded px-3 mt-3 mb-6" />
+        <h3 className="text-white font-poppins font-semibold">
+          Classes Attended:
+        </h3>
+        <div className="flex items-center justify-start mb-6">
+          <input type="text" className="rounded px-3 my-3 mr-3 w-16" />
+          <h3 className="text-white font-poppins"> Out Of</h3>
+          <input type="text" className="rounded px-3 ml-3 my-3 w-16" />
+        </div>
+        <button
+          className="w-20 h-12 text-black bg-accent rounded-lg font-poppins font-semibold"
+          onClick={toggleAddSub}
+        >
+          click
+        </button>
+      </form>
+    </Popup>
+  ) : (
+    ""
+  );
   return (
     <div>
       {subPopup}
