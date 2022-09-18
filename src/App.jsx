60eed6280c3be02ba0134efc7df9d3a568ed9590
sahilyeole <sahilyeole93@gmail.com>
2022-09-18 19:52:31 +0530

    added dynamic heading

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,27 @@ function App() {
       return [...prev, object]
     })
   }
- let headCondition = true
+
+  let headCondition = true
+  let redSubs = []
   subData.map((item) => {
     const percent = (item.classesAttended / item.totalClasses )*100
     const roundedPercent = parseFloat(percent).toFixed(2)
     if (roundedPercent < 75) {
       headCondition = false
+      redSubs.push(item)
     }
 })
 
 console.log(subData)
+
   
   return (
     <div className="w-[100%]  h-[100vh]  ">
       <Navbar onClk={click} />
         {subData.length===0? (""):(
       <div className="my-8 flex justify-between items-center">
-         <Heading condition={headCondition} />
+         <Heading condition={headCondition} redSubs={redSubs}  />
         <Leave />
       </div>
         )}
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,16 +3,28 @@ import React from "react";
 const Heading = (props) => {
   let fulfill = props.condition;
 
+let redSub = []
+
+props.redSubs.map((item) => {
+  const percent = (item.classesAttended / item.totalClasses )*100
+  const roundedPercent = parseFloat(percent).toFixed(2)
+  redSub.length === 0 ?(
+    redSub.push(`${item.name} - ${roundedPercent}%`)
+    ):(
+      redSub.push(` , ${item.name} - ${roundedPercent}%`)
+    )
+})
+
   const returnFulfill = fulfill ? (
     <h1 className="text-greenT">
-      You fulfill the Minimum Attendance criteria 
+      You Fulfill the Minimum Attendance Criteria for all the Subjects
     </h1>
   ) : (
     <div>
     <h1 className="text-redT ">
-      You are Low on attendance
+      You are Low on Attendance for the Following Subjects
     </h1>
-
+    <p className="text-white text-[19px] mt-2 tracking-wider">{redSub}</p>
     </div>
   );
 
